c4c0f7a8c96d71dbb381fe13f9565ca005e357ab
srinidhi131 <srinidhi131@gmail.com>
2021-11-20 18:38:46 +0530

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,6 +11,7 @@ import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -57,6 +58,7 @@ class Main extends Component {
                 <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                 <Route exact path='/contactus' component={Contact}  />
                 <Route path='/menu/:dishId' component={DishWithId} />
+                <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />}  />
                 <Redirect to="/home" />
           </Switch>
         <Footer />
